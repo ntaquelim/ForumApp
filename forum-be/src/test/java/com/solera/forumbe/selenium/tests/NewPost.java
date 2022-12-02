c1678c5e4e9c590a2fa8215fc58bb190ff6985e3
Andrea Colucci <Andrea.Colucci@solera.com>
2022-12-02 15:48:19 +0100

    some base part for the newPost

--- forum-be/src/test/java/com/solera/forumbe/selenium/tests/NewPost.java
+++ forum-be/src/test/java/com/solera/forumbe/selenium/tests/NewPost.java
@@ -2,7 +2,13 @@ package com.solera.forumbe.selenium.tests;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.solera.forumbe.selenium.configurations.BrowserType;
+import com.solera.forumbe.selenium.configurations.DriverFactory;
 
 @SpringBootTest
 public class NewPost {
@@ -15,6 +21,13 @@ public class NewPost {
 
     @Test
     void test(){
-        
+        WebDriver webDriver = DriverFactory.getInstance().getDriver(BrowserType.CHROME);
+        webDriver.get("http://localhost:3000/NewPost");
+
+        WebElement title = webDriver.findElement(By.id("inputTitle"));
+
+        title.sendKeys("Correct Title");
+
+        WebElement button;//TODO the button id
     }
 }
